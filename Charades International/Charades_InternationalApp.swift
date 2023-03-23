bdd8510d6a9a14f4af01490d118227ee701cc4b5
jmarc101 <jm.prudhomme@icloud.com>
2023-03-23 08:16:33 -0400

    Initial Commit

---
+++ Charades International/Charades_InternationalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Charades_InternationalApp.swift
+//  Charades International
+//
+//  Created by Jeanmarc Prudhomme on 2023-03-23.
+//
+
+import SwiftUI
+
+@main
+struct Charades_InternationalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
